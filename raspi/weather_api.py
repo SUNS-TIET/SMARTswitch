9a7fd3de721ea8127f61d4a3307258049fbf76c8
Uday <UPSAtwal@users.noreply.github.com>
2022-12-23 13:59:16 +0530

    updated APP ID to non-descript

--- raspi/weather_api.py
+++ raspi/weather_api.py
@@ -4,7 +4,7 @@ import json
 
 BASE_URL = "https://api.agromonitoring.com/agro/1.0/weather"
 
-appid = "f7a11ade0aa0ab4b018fc115bdce8d42" #this is my appid CHANGE IT
+appid = "INSERT APP ID HERE" #this is my appid CHANGE IT
 
 def get_weather_data(lat, lon, start):
 	url = f'{BASE_URL}?lat={lat}&lon={lon}&units=metric&appid={appid}'
